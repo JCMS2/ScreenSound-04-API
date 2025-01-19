f73658acb8fbbab58bab11fc8a707e40562197ab
José carlos <159956657+JCMS2@users.noreply.github.com>
2025-01-19 13:12:02 -0300

    Update Program.cs

--- ScreenSound-04/ScreenSound-04/Program.cs
+++ ScreenSound-04/ScreenSound-04/Program.cs
@@ -1,36 +1,36 @@
-﻿using ScreenSound_04.Filtros;
+using ScreenSound_04.Filtros;
 using ScreenSound_04.Modelos;
 using System.Text.Json;
 
-/using (HttpClient client = new HttpClient())
-//{
-//    try
-//    {
-//string respota = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-//var musicas = JsonSerializer.Deserialize<List<Musica>>(respota)!;
-//LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
-//LinqOrder.ExibirOrden(musicas);
-//LinqFilter.FiltrarArtistasPorGenero(musicas, "pop");
-//LinqFilter.FiltrarMusicasPorArtistas(musicas, "U2");
+using (HttpClient client = new HttpClient())
+{
+   try
+   {
+string respota = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+var musicas = JsonSerializer.Deserialize<List<Musica>>(respota)!;
+LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+LinqOrder.ExibirOrden(musicas);
+LinqFilter.FiltrarArtistasPorGenero(musicas, "pop");
+LinqFilter.FiltrarMusicasPorArtistas(musicas, "U2");
 
-//    var MusicasPreferidasDoDaniel = new MusicasPreferidas("Daniel");
-//    MusicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[0]);
-//    MusicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[1]);
-//    MusicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[2]);
-//    MusicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[3]);
-//    MusicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[4]);
-//    MusicasPreferidasDoDaniel.ExibirMusicasFavoritas();
+   var MusicasPreferidasDoDaniel = new MusicasPreferidas("Daniel");
+   MusicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[0]);
+   MusicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[1]);
+   MusicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[2]);
+   MusicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[3]);
+   MusicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[4]);
+   MusicasPreferidasDoDaniel.ExibirMusicasFavoritas();
 
-//    MusicasPreferidasDoDaniel.GerarArquivoJson();
+   MusicasPreferidasDoDaniel.GerarArquivoJson();
 
 
 
-//}
-//catch (Exception ex)
-//{
-//    Console.WriteLine($"Temos um problema: {ex.Message}");
-//}
-//}
+}
+catch (Exception ex)
+{
+   Console.WriteLine($"Temos um problema: {ex.Message}");
+}
+}
 
 using (HttpClient Client = new HttpClient())
 {
